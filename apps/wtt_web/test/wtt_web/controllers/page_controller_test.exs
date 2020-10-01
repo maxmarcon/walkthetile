9c413437e05c2488000cce1b0c031c8deb294ac2
Max Marcon <maxmarcon@gmx.net>
2020-10-01 15:04:47 +0200

    fixed test

--- apps/wtt_web/test/wtt_web/controllers/page_controller_test.exs
+++ apps/wtt_web/test/wtt_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule WttWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Walk the tile!"
   end
 end
