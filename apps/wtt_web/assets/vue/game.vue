f78e725349c9e87be2bd23e3be9f225c80f1b56c
Max Marcon <maxmarcon@gmx.net>
2020-10-01 21:15:40 +0200

    showing players

--- apps/wtt_web/assets/vue/game.vue
+++ apps/wtt_web/assets/vue/game.vue
@@ -1,9 +1,15 @@
 <template>
     <div class="container-fluid">
-        <div v-for="row in boardSize" class="row border-left border-top border-dark" :class="row === boardSize ? 'border-bottom' : ''">
-            <div v-for="col in boardSize" 
-                 class=" col border-right border-dark text-break"
+        <div v-for="row in boardSize" class="d-flex border-left border-top border-dark"
+             style="width: 100%; height: 60px"
+             :class="row === boardSize ? 'border-bottom' : ''">
+            <div v-for="col in boardSize"
+                 class="border-right border-dark" style="width: 10%"
                  :class="tileClasses(col, row)">
+                <span v-for="player in boardElem(col, row).players" class="badge"
+                      :class="player.status === 'alive' ? 'badge-primary' : 'badge-danger'">
+                    {{ player.name }}
+                </span>
             </div>
         </div>
     </div>
@@ -11,27 +17,37 @@
 <script>
 const BOARD_SIZE = 10
 const boardIndex = (x, y) => (y - 1) * BOARD_SIZE + x - 1
+const INITIAL_BOARD = new Array(BOARD_SIZE * BOARD_SIZE).fill({
+    players: [],
+    wall: false
+})
+
 
 export default {
     data: () => ({
         boardSize: BOARD_SIZE,
-        board: new Array(BOARD_SIZE * BOARD_SIZE).fill({
-            players: [],
-            wall: false
-        })
+        board: INITIAL_BOARD
     }),
     mounted() {
-        this.updateBoard()
+        // this.updateBoard()
+        setInterval(this.updateBoard, 1000)
     },
     methods: {
         tileClasses(x, y) {
-            const tile = this.tile(x, y)
+            const tile = this.boardElem(x, y)
             return tile.wall ? 'bg-danger' : ''
         },
-        tile(x, y) {
+        boardElem(x, y) {
             return this.board[boardIndex(x, y)]
         },
         async updateBoard() {
+            for (let i in this.board) {
+                this.board[i] = {
+                    players: [],
+                    wall: false
+                }
+            }
+
             const response = await this.axios.get("/board")
 
             for (let board_el of response.data) {
