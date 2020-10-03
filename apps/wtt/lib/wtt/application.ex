688fbcc47d52e133fb6244f7bf7ca2da43ff8b6d
Max Marcon <maxmarcon@gmx.net>
2020-10-03 05:03:08 +0200

    removed comment

--- apps/wtt/lib/wtt/application.ex
+++ apps/wtt/lib/wtt/application.ex
@@ -1,7 +1,6 @@
 defmodule Wtt.Application do
   import Wtt.Board
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
+
   @moduledoc false
 
   use Application
